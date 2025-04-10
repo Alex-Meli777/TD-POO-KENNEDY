1fab7bcaf4569ea6776b3dbfef422ea5f42439e5
Alex-Meli777 <126722363+Alex-Meli777@users.noreply.github.com>
2025-04-10 02:40:45 +0100

    Delete Kennedy directory

--- Kennedy/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.kennedy.kennedy {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.kennedy.kennedy to javafx.fxml;
-    exports com.kennedy.kennedy;
-}
